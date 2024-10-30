5fbc68f988ebd511a4b2d153bf34cda143f72644
Harini-Balasubramani <166095905+Harini-Balasubramani@users.noreply.github.com>
2024-10-30 05:36:50 +0000

    added comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+int main(){
+    std::cout << "Hi there" << std:: endl;
+    //cout-character o/p, standard lib, object from iostream is used to print out msg. :: scope resolution character, endl object is end line.
+    std::cout<<std::endl;
+    return(0);
+    //0 means no error.
+}
--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -3,11 +3,11 @@
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-#include <string>
+#include <string> //to get user i/p string
 
 int main(){
-    std::string str;
-    std::cin >> str;
+    std::string str; //str to hold user i/p
+    std::cin >> str; //cin object to store single word user i/p in str
     std::cout << str;
 
     std::cout << std::endl << std::endl;
